652fefcc7d5d75efebba7f71c49064d97b85c8e1
hitschman <cfallegger@gmail.com>
2017-08-14 14:44:54 +0200

    add some comments

--- MakePlayArduino.ino
+++ MakePlayArduino.ino
@@ -8,11 +8,16 @@ int const outBluetoothLED = 13;
 char bleServiceUUID[40] = "025A7775-49AA-42BD-BBDB-E2AE77782966";
 char positionCharUUID[40] = "F38A2C23-BC54-40FC-BED0-60EDDA139F47";
 
-/* Sensors */
+/* Sensors
+ *  You can add several sensor. If you have more than 4 sensor 
+ *  just add a new instance of an 'TiltSensor' object and increase
+ *  the value 'qtyOfSensors'.
+ *  
+ * TiltSensor(Pin, SensorPoints, SensorBreak)
+ */
 int const qtyOfSensors = 4;
 TiltSensor sensors[qtyOfSensors] = 
 {
-  //TiltSensor(Port, Value, LockTime) *Value 0->(-1 Live, >0 Points)
   TiltSensor(5, 0, 1000),
   TiltSensor(6, 1000, 1000),
   TiltSensor(7, 2000, 1000),
